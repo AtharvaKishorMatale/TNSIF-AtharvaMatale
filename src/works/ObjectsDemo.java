bccc9b692eb8703ded872f55b315035e9cdeb0fb
atharvamatale <142651473+atharvamatale@users.noreply.github.com>
2024-07-05 20:50:53 +0530

    Objects

---
+++ src/works/ObjectsDemo.java
@@ -0,0 +1,14 @@
+package works;
+
+public class ObjectsDemo {
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b =4;
+		int c = a+b;
+		
+		System.out.println("sum of two number is" + c);
+	}
+	}
+
+
