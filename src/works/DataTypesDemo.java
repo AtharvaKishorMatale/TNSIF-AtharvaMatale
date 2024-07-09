0363921a01a677f0c03764720a7704ce8e57c4db
atharvamatale <142651473+atharvamatale@users.noreply.github.com>
2024-07-09 10:20:12 +0530

    Datatypesdemo

---
+++ src/works/DataTypesDemo.java
@@ -0,0 +1,19 @@
+package works;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int value1 = 9/2;
+	     float value2 = 101f/61f;
+         double value3 = 10d/6d;
+
+	        System.out.println("value 1=" + value1);
+	        System.out.println("value 1=" + value2);
+	        System.out.println("value 1=" + value3);
+	    
+
+
+	}
+
+}
